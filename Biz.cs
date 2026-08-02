a549bd3eedb590840c78a22b5e18109f9ee62804
Shape581 <severusrogue5008@gmail.com>
2026-08-02 18:34:06 +0200

    C-sharp refactor

---
+++ Biz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfRgeBizsReader
+{
+    public class Biz
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string[] Sectors { get; set; }
+        public string FormatedSectors => string.Join(", ", Sectors);
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            string str = $"Name: {Name}\nAddress: {Adress}\nCity: {City}\nPhone: {Phone}\nSectors: {string.Join(", ", Sectors)}\nEmail: {Email}";
+            return str;
+        }
+    }
+}
